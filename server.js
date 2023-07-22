4f3fb8348369e4e3973c2cb7031519755e317980
TetianaUstimevko <107365766+TetianaUstimevko@users.noreply.github.com>
2023-07-22 19:37:43 +0300

    add mangoDB and mongoose

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+import app from "./app.js";
+
+// const app = require('./app')
+
+const { DB_HOST, PORT } = process.env;
+
+dotenv.config();
+
+mongoose.connect(DB_HOST).then(() => {
+  app.listen(PORT, () => {
+  console.log(`Server running. Use our API on port: ${PORT}`)
 })
+}).catch(error => console.log(error.message))
+
+
