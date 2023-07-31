9f1a8ba68e1da49a9679a08b8aa7d3c7a76896ad
TetianaUstimevko <107365766+TetianaUstimevko@users.noreply.github.com>
2023-07-31 23:03:48 +0300

    added authentication logic

--- helpers/httpError.js
+++ helpers/httpError.js
@@ -1,6 +1,14 @@
-const httpError = (status, message) => {
+const errorMessageList = {
+    400: 'Bad Request',
+    401: 'Email or password is wrong',
+    403: 'Forbiden',
+    404: 'Not found',
+    409: 'Conflict',
+};
+
+const httpError = (status, message = errorMessageList[status]) => {
     const error = new Error(message);
-    error.status=status;
+    error.status = status;
     return error;
 }
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,21 +4,21 @@ const router = express.Router();
 
 const ctrl = require('../../controllers/contacts');
 
-const { validateBody, isValidId } = require('../../middlewares');
+const { validateBody, isValidId, authenticate } = require('../../middlewares');
 
 const {schemas} = require('../../models/contact');
 
 
-router.get('/', ctrl.getAll);
+router.get('/', authenticate, ctrl.getAll);
 
-router.get('/:contactId', isValidId, ctrl.getContactById);
+router.get('/:contactId', authenticate, isValidId, ctrl.getContactById);
 
 
-router.post('/', validateBody(schemas.addSchema), ctrl.addContact);
+router.post('/', authenticate,  validateBody(schemas.addSchema), ctrl.addContact);
 
-router.delete('/:contactId', isValidId, ctrl.removeContact);
+router.delete('/:contactId', authenticate, isValidId, ctrl.removeContact);
 
-router.put('/:contactId', validateBody(schemas.addSchema), ctrl.updateContact);
+router.put('/:contactId', authenticate, isValidId,  validateBody(schemas.addSchema), ctrl.updateContact);
 
 router.patch('/:contactId/favorite', isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateFavorite);
 
